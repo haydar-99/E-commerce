c9de4ad8d56429467689cc7992022ecc7da277c9
haydar-99 <haydaralsaadi33@gmail.com>
2022-08-06 16:14:44 +0200

    finished the producomponent, not styles added

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -5,18 +5,18 @@ import Product from './product/Product';
 
 const Products = () => {
     const Products = [
-        {name: "...", id :0 , image:  "....." },
-        {name: "...", id :1 , image:  "....." },
-        {name: "...", id :2 , image:  "....." }
+        {name: "...", id :0 , image:  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F004%2F999%2F463%2Foriginal%2Fshopping-cart-icon-illustration-free-vector.jpg&imgrefurl=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fshopping-cart-icon&tbnid=3o2OQEGe8vTipM&vet=12ahUKEwjA67G0srL5AhVEhP0HHcI6AKEQMygIegUIARDmAQ..i&docid=Ekq15ECXc5mVoM&w=1920&h=1597&q=Cart&hl=sv&ved=2ahUKEwjA67G0srL5AhVEhP0HHcI6AKEQMygIegUIARDmAQ" , price: 3, description: "This is a product" },
+        {name: "...", id :1 , image:  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F004%2F999%2F463%2Foriginal%2Fshopping-cart-icon-illustration-free-vector.jpg&imgrefurl=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fshopping-cart-icon&tbnid=3o2OQEGe8vTipM&vet=12ahUKEwjA67G0srL5AhVEhP0HHcI6AKEQMygIegUIARDmAQ..i&docid=Ekq15ECXc5mVoM&w=1920&h=1597&q=Cart&hl=sv&ved=2ahUKEwjA67G0srL5AhVEhP0HHcI6AKEQMygIegUIARDmAQ" , price: 3, description: "This is a product" },
+        {name: "...", id :2 , image:  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F004%2F999%2F463%2Foriginal%2Fshopping-cart-icon-illustration-free-vector.jpg&imgrefurl=https%3A%2F%2Fwww.vecteezy.com%2Ffree-vector%2Fshopping-cart-icon&tbnid=3o2OQEGe8vTipM&vet=12ahUKEwjA67G0srL5AhVEhP0HHcI6AKEQMygIegUIARDmAQ..i&docid=Ekq15ECXc5mVoM&w=1920&h=1597&q=Cart&hl=sv&ved=2ahUKEwjA67G0srL5AhVEhP0HHcI6AKEQMygIegUIARDmAQ" , price: 3, description: "This is a product" }
     ];
     
   return (
     <div>
-        <Grid container spacing={3} >
+        <Grid container justifyContent ='center' spacing={3} >
             {
                 Products.map( (product) =>  (
                     <Grid item key={product.id}>
-                        <Product/>
+                        <Product product={product}/>
                     </Grid>
                 ))
             }
--- client/src/components/products/product/Product.jsx
+++ client/src/components/products/product/Product.jsx
@@ -1,8 +1,28 @@
+import { Card, CardActions, CardContent, CardMedia, Typography, IconButton } from '@material-ui/core'
+import {ShoppingCart} from "@material-ui/icons"
 import React from 'react'
 
-const Product = () => {
+const Product = ({product}) => {
   return (
-    <div>Product</div>
+    <Card>
+      <CardMedia image={product.image} title= {product.name} />
+      <CardContent>
+        <div>
+          <Typography variant='h5' gutterBottom>
+            {product.name}
+          </Typography>
+          <Typography>
+            {product.price}
+          </Typography>
+          <Typography variant='h4' color='textSecondary'>{product.description}</Typography>
+        </div>
+      </CardContent>
+      <CardActions>
+        <IconButton aria-label='Add to cart'>
+            <ShoppingCart/>
+        </IconButton>
+      </CardActions>
+    </Card>
   )
 }
 
