e3e15ef9fdb52b652d4ccf622a1123555518b187
haydar-99 <haydaralsaadi33@gmail.com>
2022-09-08 22:04:51 +0200

    some styling

--- client/src/components/products/Products.jsx
+++ client/src/components/products/Products.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Grid} from "@material-ui/core"
+import {Grid, CircularProgress} from "@material-ui/core"
 import Product from './product/Product';
 // import logo from "../../assets/cartImage.jpg"
 
@@ -12,7 +12,8 @@ const Products = ({Products}) => {
     
   return (
     <div  >
-        <Grid style={{marginTop:"20px"}}  container justifyContent ='center' spacing={3} >
+        {!Products.length ? <CircularProgress style={{marginTop:"50px", marginLeft:"800px"}}/>:
+         <Grid style={{marginTop:"20px"}}  container justifyContent ='center' spacing={3} >
             {
                 Products.map( (product, index ) =>  (
                     <Grid item key={index} style={{height:"310px", width:"300px"}} >
@@ -21,7 +22,7 @@ const Products = ({Products}) => {
                 ))
             }
 
-        </Grid>
+        </Grid>}
     </div>
   )
 }
